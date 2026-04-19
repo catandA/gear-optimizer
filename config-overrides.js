6fc3be1ad6d422b53de875747c75389f59d1d987
catand <3047354896@qq.com>
2026-04-19 13:47:47 +0800

    撤销缓存优化配置

--- config-overrides.js
+++ config-overrides.js
@@ -11,35 +11,5 @@ module.exports = function override(config, env) {
         });
         config.output['globalObject'] = 'this';
 
-        if (env === 'production') {
-
-                const isFilenameSet = config.output.filename && config.output.filename.includes('hash');
-                const isChunkFilenameSet = config.output.chunkFilename && config.output.chunkFilename.includes('hash');
-
-                if (!isFilenameSet || !isChunkFilenameSet) {
-                        if (!isFilenameSet) {
-                                config.output.filename = 'static/js/[name].[contenthash:8].js';
-                        }
-                        if (!isChunkFilenameSet) {
-                                config.output.chunkFilename = 'static/js/[name].[contenthash:8].chunk.js';
-                        }
-                }
-
-                const miniCssExtractPlugin = config.plugins.find(
-                        plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
-                );
-                if (miniCssExtractPlugin) {
-                        const cssFilename = miniCssExtractPlugin.options.filename;
-                        const cssChunkFilename = miniCssExtractPlugin.options.chunkFilename;
-
-                        if (!cssFilename || !cssFilename.includes('[contenthash]')) {
-                                miniCssExtractPlugin.options.filename = 'static/css/[name].[contenthash:8].css';
-                        }
-                        if (!cssChunkFilename || !cssChunkFilename.includes('[contenthash]')) {
-                                miniCssExtractPlugin.options.chunkFilename = 'static/css/[name].[contenthash:8].chunk.css';
-                        }
-                }
-        }
-
         return config;
 }
